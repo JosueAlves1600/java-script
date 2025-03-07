184a765983ee6dfeb0866768f7b508a357aa8c13
Josué Alves <josuealves13silva@gmail.com>
2025-03-07 12:06:34 -0300

    1 TRI-AC1 02

---
+++ 1 tri-AC1 02/JS/script.js
@@ -0,0 +1,4 @@
+alert('Vamos converter a temperatura em Celcius para Fahrenheit!');
+TempCelsius=parseFloat(prompt('Digite a temperatura em Celsius'));
+TempFahrenheit=((TempCelsius*1.8)+32);
+alert(`O valor de da Temperatura de Fahrenheit é: ${TempFahrenheit} `);
