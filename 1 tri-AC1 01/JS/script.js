b2738b66cc62fe4e967fc22e7b363318c095b92e
Josué Alves <josuealves13silva@gmail.com>
2025-03-07 12:05:03 -0300

    1 TRI- AC1 01

---
+++ 1 tri-AC1 01/JS/script.js
@@ -0,0 +1,5 @@
+alert('Vamos calcular a Área de um retângulo');
+ValordaBase=parseFloat(prompt('Coleque o Valor da Base'));
+ValordaAltura=parseFloat(prompt('Coloque o Valor da Altura'));
+ValordaÁrea=ValordaBase*ValordaAltura;
+alert(`O Valor de sua bae é: ${ValordaÁrea}`);
