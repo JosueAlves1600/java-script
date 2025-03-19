e069c0700682ff741a6cb82b3373dc31c5950173
Josué Alves <josuealves13silva@gmail.com>
2025-03-19 08:03:43 -0300

    aula de calcular o salário do trabalhador

---
+++ frondend 04/js/script.js
@@ -0,0 +1,20 @@
+function calcularsalariobruto(){
+    mensagem='';
+    n1=parseFloat(document.getElementById('n1').value);
+    n2=parseFloat(document.getElementById('n2').value);
+    salarioBruto=(n1*n2).toFixed(1);
+    mensagem='seu salario bruto';
+    document.getElementById('resposta').innerHTML=mensagem;
+}
+    if(media<5000){
+        impostomenor=(salarioBruto%15);
+        mensagem=`Imposto: ${media}. Imposto menor que 5000!`
+        }else{
+            impostomaior=(salarioBruto%22);
+        mensagem=`Imposto: ${media}. Imposto maior que 5000!`
+}
+{
+    
+}
+
+
