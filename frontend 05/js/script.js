6fe9915cfe2215e92ac468733691cf9cb2122b65
Josué Alves <josuealves13silva@gmail.com>
2025-03-21 08:11:37 -0300

    frontend 05

---
+++ frontend 05/js/script.js
@@ -0,0 +1,25 @@
+function calcularsalariobruto(){
+    
+    imposto=0;
+    sl=0;
+    n1=parseFloat(document.getElementById('n1').value);
+    n2=parseFloat(document.getElementById('n2').value);
+    sb=n1*n2.toFixed(1);
+
+    if(sb<=1500){
+        imposto=0;
+    }else if(sb<=3000){
+        imposto=sb*0.1;
+    }else if(sb<=5000){
+        imposto=sb*0.15;
+    }else if(sb<=10000){
+        imposto=sb*0.22;
+    }else{
+        imposto=sb*0.27;
+    }
+    sl=sb-imposto;
+    document.getElementById("resposta").innerHTML=
+    `Salário Bruto: R$${sb}<br>Imposto: R$${imposto}<br>
+    Salário Líquido: R$${sl}` ;
+    
+}
