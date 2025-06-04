e131e14f6579e3f92a6d13a4e09dd661c653e788
Josué Alves <josuealves13silva@gmail.com>
2025-06-04 07:38:04 -0300

    exercicio while

---
+++ laçosrepetiçao/js/script.js
@@ -0,0 +1,10 @@
+function fatorial(){
+    valor=parseInt(document.getElementById('valor').value);
+    resp=1;
+    i=1;
+    while(i<=valor){
+    resp=resp*i;
+    i++;
+    }
+    document.getElementById('resposta').innerHTML=resp;
+    }
