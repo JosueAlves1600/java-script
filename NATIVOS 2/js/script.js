d6417db8bc13f0436da361140b6e46659c06db4a
Josué Alves <josuealves13silva@gmail.com>
2025-08-29 13:19:34 -0300

    Add files via upload

---
+++ NATIVOS 2/js/script.js
@@ -0,0 +1,7 @@
+//x=new String('Criando uma string');
+//alert(x.length); // length: quantidade de caracteries; 
+//alert(x.charAt(4)); //posições da palavra
+//alert(x.charCodeAt(4));
+//alert(x.concat(' - junatando textos'));
+
+alert(String.fromCharCode(66));
