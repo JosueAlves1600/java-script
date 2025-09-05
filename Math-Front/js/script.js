40757637fcf7a77d48f7d36eed0ef25fee895bad
Josué Alves <josuealves13silva@gmail.com>
2025-09-05 12:43:33 -0300

    Add files via upload

---
+++ Math-Front/js/script.js
@@ -0,0 +1,27 @@
+//alert(Math.PI); //valor de PI
+
+//alert(Math.abs(5.9)); //número absoluto
+
+//alert(Math.max(5.9,6,12,-80)) //menor número (será o 12)
+
+//alert(Math.round(12.6)); //inteiro mais próximo
+
+//alert(Math.floor(45.5)); //arredonda para baixo
+
+//alert(Math.ceil(45.5)); //arredonda para cima
+
+//alert(Math.pow(12,3)); //12 elevado a terceira potência
+
+//alert(Math.sqrt(16)); //raiz quadrada de 16
+
+//alert(Math.floor((Math.random()*6)+1)); //dado de 1 a 6
+
+
+aspirina=7.30;
+quantidade1= 1;
+quantidade2= 2;
+
+if(quantidade1 * aspirina == 7.30 ){
+    alert("Aspirina com 1 quantidade é 7.30R$");
+} else(quantidade2 * aspirina == 14);
+   alert("Aspirina com 2 quantidade é 14R$");
