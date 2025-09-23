61b74e4d793ae61cfe8db78f4089d1296f71ad7d
Josué Alves <josuealves13silva@gmail.com>
2025-09-23 07:52:00 -0300

    Array

---
+++ Array/js/script.js
@@ -0,0 +1,13 @@
+const carrosPasseio=['Onix','Argo','Kwid'];
+
+const carrosSUV=[
+    'T-Cross',
+    'Renegade',
+    'Tracker'
+];
+
+const carrosPickUp=[];
+carrosPickUp[0]='RAM';
+carrosPickUp[1]='Toro';
+carrosPickUp[2]='Montana';
+alert(carrosPickUp);
