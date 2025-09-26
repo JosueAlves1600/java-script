532e5de9f104e32b821c0233f6702138f19844ee
Josué Alves <josuealves13silva@gmail.com>
2025-09-26 13:07:23 -0300

    Array 2

---
+++ Array 2/js/script.js
@@ -0,0 +1,46 @@
+//const frutas=['Banana','Laranja','Maça','Manga'];
+//alert(frutas.lenght); ADIONA ELEMENTO
+//alert(frutas[0]);
+//alert(frutas.lenght-1)
+//
+//const frutas=['Banana','Laranja','Maça','Manga'];
+//alert(frutas);
+//frutas.pop()
+//alert(frutas); REMOVE ULTIMO ELEMENTO
+//
+//const frutas=['Banana','Laranja','Maça','Manga'];
+//alert(frutas);
+//frutas.shift() REMOVE PRIMEIRO ELEMENTO
+//alert(frutas);
+//
+//const frutas=['Banana','Laranja','Maça','Manga'];
+//alert(frutas);
+//frutas.unshift() 
+//alert(frutas);
+//
+//const frutas=['Banana','Laranja','Maça','Manga'];
+//alert(frutas);
+//delete frutas[1]; REMOVE O ELEMENTO 1 A LARANJA
+//alert(frutas);
+//alert(frutas.lenght); AINDA POSSUI 4 ELEMENTOS, ELIMINA APENAS O ELEMENTO, NÃO O COMPARTIMENTO
+//
+//const frutas=['Banana','Laranja','Maça','Manga']
+//alert(frutas);
+//frutas.splice(2,0,'Kiwi','Limão') COLOCA O ELEMENTO, PRIMEIRO COLOCA A POSIÇÃO E DEPOIS ELIMINA O ELEMENTO QUE VEM DEPOIS DO ELEMENTO INSIRIDO
+//alert(frutas);
+//
+//const frutas=['Banana','Laranja','Maça','Manga'];
+//const verduras =['Couve','Verduras'];
+//const comidas=frutas.concat(verduras); CONCATENA E TORNA UM NOVO ARRAY
+//alert(comidas);
+//
+//const comidas=['Banana','Laranja','Maça','Manga','Couve','Brocolis'];
+//const verduras=comidas.slice(4); FAZ UM VETOR NOVO A PARTIR DA POSIÇÃO DETERMINADA 
+//alert(verduras);
+//alert(comidas);
+//
+//const comidas=['Banana','Laranja','Maça','Manga'];
+//comidas.sort(); COLOCA EM ORDEM CRESCENTE(ALFABÉTICA)
+//alert(comidas);
+//comidas.reverse(); COLOCA EM ORDEM DECRESCENTE
+//alert(comidas);
