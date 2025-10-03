7a3dd8342428cf3ee93285fdc1243000465d64a3
Josué Alves <josuealves13silva@gmail.com>
2025-10-03 12:52:16 -0300

    Exercicio Array 3/10

---
+++ ExercicioArray/js/script.js
@@ -0,0 +1,53 @@
+//Exercicio 1:
+/*
+const VetorA = ["João","Alex","Monica","Jairo","Igor"];
+const VetorB = ["Nicolas","Caua","Eduardo","Oswaldo","Saulo"];
+let VetorC = [VetorA,VetorB];
+alert(VetorC);
+*/
+//Exercicio 2:
+/*
+const VetorA = [
+               "Claudio",
+               "Claudete",
+               "Tassiano",
+               "Ronaldo",
+               "Leonardo",
+               "Iago",
+               "Ana",
+               "Beatriz",
+               "Elizabeth",
+               "Gabriel",
+               "Flavio",
+               "Paulo",
+               "Daniel",
+               "Leticia",
+               "Pablo",
+               "Theo",
+               "Eduarda",
+               "Veronica",
+               "Heitor",
+               "Henrique"
+               ]; 
+ 
+const NovoVetor = VetorA.slice(10);
+alert(NovoVetor);
+*/
+//Exercicio 3:
+/*
+const VetorA = [1,2,4,8,16,32,64,128,256,512];
+const VetorB = VetorA.reverse();
+alert(VetorB);
+*/
+//Outra maneira de fazr o Exercicio 3:
+/*
+const numeros = [12, 57, 34, 89, 25, 47, 61, 93, 18, 73];
+const numerosInvertidos = [];
+i = 9
+numeros.forEach((numero) => {
+numerosInvertidos[i] = numero;
+i--;
+}
+);
+document.writeln(numerosInvertidos);
+*/
